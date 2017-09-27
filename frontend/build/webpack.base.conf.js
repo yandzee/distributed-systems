ea74a76d9ecfa3646afa8113132544c85cacc213
yandzee <yandzeek@gmail.com>
2017-09-27 20:06:12 +0300

    wip: fix

--- frontend/build/webpack.base.conf.js
+++ frontend/build/webpack.base.conf.js
@@ -2,7 +2,6 @@ var path = require('path')
 var utils = require('./utils')
 var config = require('../config')
 var vueLoaderConfig = require('./vue-loader.conf')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
